a874a850b872917a917a3c1bcdb0641613562572
choijw1004 <97choijw@gmail.com>
2025-02-05 15:12:49 +0900

    20250205 Algorithm: BOJ_11000_강의실 배정

---
+++ BOJ_11000_강의실배정/Main.java
@@ -0,0 +1,37 @@
+package BOJ_11000_강의실배정;
+import java.util.*;
+
+public class Main {
+    public static class Lecture{
+        int start;
+        int end;
+
+        public Lecture(int a, int b){
+            this.start = a;
+            this. end = b;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        List<Lecture> lectures = new ArrayList<>();
+
+        for(int i = 0 ; i < N; i++){
+            lectures.add(new Lecture(sc.nextInt(), sc.nextInt()));
+        }
+
+        Collections.sort(lectures, (s1, s2) ->  s1.start - s2.start);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(Lecture lecture : lectures){
+            if(!pq.isEmpty()&& pq.peek() <= lecture.start){
+                pq.poll();
+            }
+            pq.offer(lecture.end);
+        }
+        System.out.println(pq.size());
+    }
+}
