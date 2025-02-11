db0da7d7d3af35f0e355ec54c95aebb8f27854c8
choijw1004 <97choijw@gmail.com>
2025-02-11 16:06:05 +0900

    20250211 Algorithm: BOJ_3151_합0

---
+++ BOJ_3151_합0/Main.java
@@ -0,0 +1,51 @@
+package BOJ_3151_합0;import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] nums = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        Arrays.sort(nums);
+        long count = 0;
+
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                int target = -(nums[i] + nums[j]);
+                int lower = lowerBound(nums, j + 1, N, target);
+                int upper = upperBound(nums, j + 1, N, target);
+                count += (upper - lower);
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    static int lowerBound(int[] arr, int left, int right, int target) {
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    static int upperBound(int[] arr, int left, int right, int target) {
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
