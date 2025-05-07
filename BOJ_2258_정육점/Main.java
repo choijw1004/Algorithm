1da1503184b0974a5f62210e0c2773a3112bc1d4
choijw1004 <97choijw@gmail.com>
2025-05-07 09:33:46 +0900

    20250507 Algorithm: BOJ_2258_정육점

--- BOJ_2258_정육점/Main.java
+++ BOJ_2258_정육점/Main.java
@@ -1,42 +1,55 @@
 package BOJ_2258_정육점;
+
 import java.util.*;
+
 public class Main {
-    public static int solution(int[][] meats, int m){
-        int ans = 0;
-        int n = meats.length;
-        //가격 낮은 순 무게 높은 순
-        Arrays.sort(meats, (a,b) -> {
-            if(a[1] == b[1]) return b[0] - a[0];
+    private static int solution(int n, int m, int[][] meats) {
+        int total = 0;
+        int weightSum = 0;
+        int priceSum = 0;
+        int prePrice = -1;
+        int minCost = Integer.MAX_VALUE;
+
+        Arrays.sort(meats, (a, b) -> {
+            if (a[1] == b[1]) return b[0] - a[0];
             return a[1] - b[1];
         });
-        List<Integer> list = new ArrayList<>();
-        int prePrice = 0;
-        int weihgt = 0;
 
-        for(int[] meat : meats){
-            int w = meat[0];
-            int currPrice = meat[1];
+        for (int i = 0; i < n; i++) {
+            total += meats[i][0];
+        }
 
-            if(currPrice == prePrice) continue;
-            else{
+        if (total < m) {
+            return -1;
+        }
 
+        for (int[] cur : meats) {
+            weightSum += cur[0];
+            if (cur[1] != prePrice) {
+                prePrice  = cur[1];
+                priceSum = cur[1];
+            } else {
+                priceSum += cur[1];
+            }
+            if (weightSum >= m) {
+                minCost = Math.min(minCost, priceSum);
             }
         }
 
-
-        return ans;
+        return minCost;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int m = sc.nextInt();
+
         int[][] meats = new int[n][2];
-        int max = 0;
-        for(int i = 0 ; i < n; i++){
+        for (int i = 0; i < n; i++) {
             meats[i][0] = sc.nextInt();
             meats[i][1] = sc.nextInt();
-            max += meats[i][0];
         }
-        System.out.println(m > max ? -1 : solution(meats, m) );
+
+        System.out.println(solution(n, m, meats));
     }
 }
