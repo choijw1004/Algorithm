d42999f24e686a42a3508e814e5bd0793b6ffaab
choijw1004 <97choijw@gmail.com>
2025-07-04 16:08:15 +0900

    20250704 Algorithm: BOJ_9251_LCS

--- BOJ_9251_LCS/Main.java
+++ BOJ_9251_LCS/Main.java
@@ -6,23 +6,20 @@ public class Main {
     static String a, b;
 
     public static int calcuLCS(String a, String b){
-        int aLen = a.length();
-        int bLen = b.length();
+        int n = a.length();
+        int m = b.length();
 
-        int res = -1;
-
-        for (int i = 1; i < aLen + 1; i++) {
-            for (int j = 1; j < bLen + 1; j++) {
-                if (a.charAt(i - 1) == b.charAt(j - 1)) {
-                    DP[i][j] = DP[i - 1][j - 1] + 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(a.charAt(i-1) == b.charAt(j-1)){
+                    DP[i][j] = DP[i-1][j-1] + 1;
                 }
-                else {
-                    DP[i][j] = Math.max(DP[i - 1][j], DP[i][j - 1]);
-                }
-                res = Math.max(DP[i][j], res);
+                else DP[i][j] = Math.max(DP[i-1][j], DP[i][j-1]);
             }
         }
-        return res;
+
+        return DP[n][m];
+
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
