e7ed75f221e7a4222855ad435835bceee6550cd8
choijw1004 <97choijw@gmail.com>
2026-02-04 12:55:37 +0900

    feat: BOJ_12865_평범한_배낭

---
+++ src/ver2/BOJ_12865_평범한_배낭/README.md
@@ -0,0 +1,56 @@
+# BOJ_12865_평범한_배낭
+
+## 문제 링크
+https://www.acmicpc.net/problem/12865
+
+## 카테고리
+`DP` `냅색` 
+
+
+## 코드
+```java
+package ver2.BOJ_12865_평범한_배낭;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int max = sc.nextInt();
+
+        int[][] items = new int[n][2];
+        int[] dp = new int[max + 1];
+        dp[0] = 0;
+
+        for(int i = 0;  i < n; i++){
+            items[i][0] = sc.nextInt();
+            items[i][1] = sc.nextInt();
+        }
+
+        for(int[] item : items){
+            int cost = item[0];
+            int weight = item[1];
+
+            for(int j = max; j - cost >= 0; j--){
+                dp[j] = Math.max(dp[j], dp[j-cost] + weight);
+            }
+        }
+        System.out.println(dp[max]);
+
+    }
+}
+
+/*
+# 카테고리
+DP, 냅색
+
+# 접근 방식
+냅색 알고리즘으로 해결했다.
+
+# 문제 링크
+https://www.acmicpc.net/problem/12865
+ */
+
+
+```
--- src/ver2/Main.java
+++ src/ver2/Main.java
@@ -1,43 +1,51 @@
 package ver2;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int max = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
 
-        int[][] items = new int[n][2];
-        int[] dp = new int[max + 1];
-        dp[0] = 0;
-
-        for(int i = 0;  i < n; i++){
-            items[i][0] = sc.nextInt();
-            items[i][1] = sc.nextInt();
+        for(int i = 0 ; i < n; i++){
+            arr[i] = sc.nextInt();
         }
 
-        for(int[] item : items){
-            int cost = item[0];
-            int weight = item[1];
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-            for(int j = max; j - cost >= 0; j--){
-                dp[j] = Math.max(dp[j], dp[j-cost] + weight);
+        int start = 0;
+        int end = 0;
+        int ans = -1;
+
+        while(end < n){
+
+            map.put(arr[end], map.getOrDefault(arr[end], 0) + 1);
+
+            while(map.get(arr[end]) > k){
+                map.put(arr[start], map.getOrDefault(arr[start], 0) -1);
+                start++;
+
             }
+            ans = Math.max(ans, end - start + 1);
+            end++;
+
         }
-        System.out.println(dp[max]);
+
+        System.out.println(ans);
 
     }
 }
 
 /*
 # 카테고리
-DP, 냅색
+투 포인터, 슬라이딩 윈도우
 
 # 접근 방식
-냅색 알고리즘으로 해결했다.
+고정되지않은 슬라이딩 윈도우 문제이다.
 
 # 문제 링크
-https://www.acmicpc.net/problem/12865
+https://www.acmicpc.net/problem/20922
  */
-
