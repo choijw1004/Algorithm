6d757d4f84a6e4730739f26935e851dd4c912fc0
choijw1004 <97choijw@gmail.com>
2026-02-04 12:49:07 +0900

    feat: BOJ_30804_과일_탕후루

---
+++ src/ver2/BOJ_30804_과일_탕후루/README.md
@@ -0,0 +1,66 @@
+# BOJ_30804_과일_탕후루
+
+## 문제 링크
+https://www.acmicpc.net/problem/30804
+
+## 카테고리
+`투 포인터` `슬라이딩 윈도우` 
+
+
+## 코드
+```java
+package ver2.BOJ_30804_과일_탕후루;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0 ; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int start = 0;
+        int end = 0;
+        int ans = -1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        while(end < n){
+            map.put(arr[end], map.getOrDefault(arr[end], 0) + 1);
+
+            while(map.size() > 2){
+                map.put(arr[start], map.getOrDefault(arr[start],0) - 1);
+                if(map.get(arr[start]) == 0) {
+                    map.remove(arr[start]);
+                }
+                start++;
+            }
+
+            ans = Math.max(end - start + 1, ans);
+            end++;
+
+        }
+
+        System.out.println(ans);
+
+    }
+}
+
+/*
+# 카테고리
+투 포인터, 슬라이딩 윈도우
+
+# 접근 방식
+투 포인터와 슬라이딩 윈도우를 활용하여 해결했다.
+
+# 문제 링크
+https://www.acmicpc.net/problem/30804
+ */
+
+
+```
--- src/ver2/Main.java
+++ src/ver2/Main.java
@@ -1,53 +1,42 @@
 package ver2;
 
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        int max = sc.nextInt();
 
-        for(int i = 0 ; i < n; i++){
-            arr[i] = sc.nextInt();
+        int[][] items = new int[n][2];
+        int[] dp = new int[max + 1];
+        dp[0] = 0;
+
+        for(int i = 0;  i < n; i++){
+            items[i][0] = sc.nextInt();
+            items[i][1] = sc.nextInt();
         }
 
-        int start = 0;
-        int end = 0;
-        int ans = -1;
-        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int[] item : items){
+            int cost = item[0];
+            int weight = item[1];
 
-        while(end < n){
-            map.put(arr[end], map.getOrDefault(arr[end], 0) + 1);
-
-            while(map.size() > 2){
-                map.put(arr[start], map.getOrDefault(arr[start],0) - 1);
-                if(map.get(arr[start]) == 0) {
-                    map.remove(arr[start]);
-                }
-                start++;
+            for(int j = max; j - cost >= 0; j--){
+                dp[j] = Math.max(dp[j], dp[j-cost] + weight);
             }
-
-            ans = Math.max(end - start + 1, ans);
-            end++;
-
         }
-
-        System.out.println(ans);
+        System.out.println(dp[max]);
 
     }
 }
 
 /*
 # 카테고리
-투 포인터, 슬라이딩 윈도우
+DP, 냅색
 
 # 접근 방식
-투 포인터와 슬라이딩 윈도우를 활용하여 해결했다.
+냅색 알고리즘으로 해결했다.
 
 # 문제 링크
-https://www.acmicpc.net/problem/30804
+https://www.acmicpc.net/problem/12865
  */
-
