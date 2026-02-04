a6a24db04724183e322b2362477b2311ebd82990
choijw1004 <97choijw@gmail.com>
2026-02-04 12:31:59 +0900

    feat: BOJ_1967_트리의_지름

---
+++ src/ver2/BOJ_16401_과자_나눠주기/Main.java
@@ -0,0 +1,60 @@
+package ver2.BOJ_16401_과자_나눠주기;
+
+import java.util.Scanner;
+
+
+public class Main {
+    static int n,m;
+    static int[] cookies;
+
+    private static boolean canGive(int mid){
+        int cnt = 0;
+
+        for(int cookie : cookies){
+            cnt += cookie / mid;
+        }
+
+        return cnt >= n ? true : false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        int start = 1;
+        int end = -1;
+        int ans = 0;
+
+        cookies = new int[m];
+
+        for(int i = 0 ; i < m; i++){
+            int c = sc.nextInt();
+            end = Math.max(c, end);
+            cookies[i] = c;
+        }
+
+        while(start <= end){
+            int mid = (start + end) / 2;
+
+            if(canGive(mid)){
+                ans = mid;
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        System.out.println(ans);
+    }
+}
+/*
+# 카테고리
+이분탐색
+
+# 접근 방식
+길이의 `최댓값`을 구하는 문제이다. 최댓값을 구해주는 이분탐색의 로직을 작성하였는데,
+만약 canGive 메서드가 true를 반환하면 start의 값을 mid + 1로 아니라면 mie -1 로 간격을 줄여가면서 탐색하는 방식으로 해결했다.
+
+# 문제 링크
+https://www.acmicpc.net/problem/16401
+ */
--- src/ver2/Main.java
+++ src/ver2/Main.java
@@ -1,60 +1,76 @@
 package ver2;
 
-import java.util.Scanner;
+import java.util.*;
 
+class Node{
+    int to;
+    int w;
 
-public class Main {
-    static int n,m;
-    static int[] cookies;
-
-    private static boolean canGive(int mid){
-        int cnt = 0;
-
-        for(int cookie : cookies){
-            cnt += cookie / mid;
-        }
-
-        return cnt >= n ? true : false;
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        n = sc.nextInt();
-        m = sc.nextInt();
-        int start = 1;
-        int end = -1;
-        int ans = 0;
-
-        cookies = new int[m];
-
-        for(int i = 0 ; i < m; i++){
-            int c = sc.nextInt();
-            end = Math.max(c, end);
-            cookies[i] = c;
-        }
-
-        while(start <= end){
-            int mid = (start + end) / 2;
-
-            if(canGive(mid)){
-                ans = mid;
-                start = mid + 1;
-            }
-            else{
-                end = mid - 1;
-            }
-        }
-        System.out.println(ans);
+    public Node(int to, int w){
+        this.to = to;
+        this.w = w;
     }
 }
+public class Main {
+    static int n;
+    static List<Node>[] tree;
+
+    private static int[] bfs(int start){
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(start,0));
+        boolean[] visited = new boolean[n+1];
+        visited[start] = true;
+
+        int max = 0;
+        int point = 0;
+
+        while(!q.isEmpty()){
+            Node curr = q.poll();
+
+            if(curr.w > max){
+                max = curr.w;
+                point = curr.to;
+            }
+
+            for(Node next : tree[curr.to]){
+                if(!visited[next.to]){
+                    visited[next.to] = true;
+                    q.offer(new Node(next.to, curr.w + next.w));
+                }
+            }
+        }
+        return new int[]{point,max};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        tree = new ArrayList[n+1];
+        for(int i = 0; i <= n; i++) tree[i] = new ArrayList<>();
+
+        for(int i = 0; i < n-1; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            tree[a].add(new Node(b,c));
+            tree[b].add(new Node(a,c));
+        }
+
+        int[] p1 = bfs(1);
+        int[] p2 = bfs(p1[0]);
+
+        System.out.println(p2[1]);
+    }
+}
+
 /*
 # 카테고리
-이분탐색
+트리, BFS
 
 # 접근 방식
-길이의 `최댓값`을 구하는 문제이다. 최댓값을 구해주는 이분탐색의 로직을 작성하였는데,
-만약 canGive 메서드가 true를 반환하면 start의 값을 mid + 1로 아니라면 mie -1 로 간격을 줄여가면서 탐색하는 방식으로 해결했다.
-
+루트에서 하나의 노드의 최장 거리를 가진 노드를 찾고, 그 노드에서부터 가장 먼거리의 노드를 찾아서 반환하는 방식으로 해결했다.
 # 문제 링크
-https://www.acmicpc.net/problem/16401
+https://www.acmicpc.net/problem/1967
  */
