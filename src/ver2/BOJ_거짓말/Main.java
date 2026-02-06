ae7bc985dd9239b910abc72522ea1652522f6f93
choijw1004 <97choijw@gmail.com>
2026-02-06 11:58:48 +0900

    feat: 알고리즘_문제해결

--- src/ver2/BOJ_거짓말/Main.java
+++ src/ver2/BOJ_거짓말/Main.java
@@ -1,47 +1,46 @@
-package ver2.BOJ_거짓말;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-public class Main {
-    static int n,m;
-    static List<Integer>[] graph;
-
-    private static int find(int x){
-        if(parent[x] != x){
-            parent[x] = find(parent[x]);
-        }
-        return parent[x];
-    }
-    private static void union(int x, int y){
-        int rootX = find(x);
-        int rootY = find(y);
-
-        if(rootX != rootY) parent[rootX] = parent[rootY];
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        int c = sc.nextInt();
-        graph = new ArrayList[n+1];
-
-        for(int i = 0 ; i <= n ;i++){
-            graph[i] = new ArrayList<>();
-        }
-
-        int[] knows = new int[c];
-
-        if(c != 0){
-            for(int i = 0 ; i < c; i++){
-                knows[i] = sc.nextInt();
-            }
-        }
-
-        for(int i = 0 ; i < m; i++){
-            for(int j = 0 ; j < )
-        }
-
-    }
-}
+//package ver2.BOJ_거짓말;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Scanner;
+//
+//public class Main {
+//    static int n,m;
+//    static List<Integer>[] graph;
+//
+//    private static int find(int x){
+//        if(parent[x] != x){
+//            parent[x] = find(parent[x]);
+//        }
+//        return parent[x];
+//    }
+//    private static void union(int x, int y){
+//        int rootX = find(x);
+//        int rootY = find(y);
+//
+//        if(rootX != rootY) parent[rootX] = parent[rootY];
+//    }
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        n = sc.nextInt();
+//        m = sc.nextInt();
+//        int c = sc.nextInt();
+//        graph = new ArrayList[n+1];
+//
+//        for(int i = 0 ; i <= n ;i++){
+//            graph[i] = new ArrayList<>();
+//        }
+//
+//        int[] knows = new int[c];
+//
+//        if(c != 0){
+//            for(int i = 0 ; i < c; i++){
+//                knows[i] = sc.nextInt();
+//            }
+//        }
+//
+//        for(int i = 0 ; i < m; i++){
+//        }
+//
+//    }
+//}
